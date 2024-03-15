31e945a378afd7d2367f69c668fa94e8ca9330f1
eduardorz <ruizzmarin20@gmail.com>
2024-03-15 20:28:13 -0300

    controlador de peliculas

--- back/src/controllers/movieController.js
+++ back/src/controllers/movieController.js
@@ -1,6 +1,6 @@
 
 const movieController = (req, res) => {
-    res.status(200).send("Estamos recibiendo una solicitud para obtener datos de las peliculas.")
+    res.status(200).send("Próximamente estarán disponibles los datos de las peliculas ...")
 };
 
 module.exports = movieController;
